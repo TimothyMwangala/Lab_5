2d917e607b8ae3d7df928404394b33f21b165f47
Timothy Mwangala <P15214646@LEC-ADMIN.dmu.ac.uk>
2018-11-12 10:48:45 +0000

    html forms/ js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+"user-strict";
+const theForm document.getElementById("the-form");
+const theDate document.getElementById("the-date");
+const getDate document.querySelector input[type="date"];
+document.getElementById("date").addEventListener("change", showDate);
+function showDate(){
+let d = new Date(getDate.value);
+}
